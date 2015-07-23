c9b186b6a9ca44ef2ef1427e6b2b24e2bc59ffe8
Sam Sheftel <mathias@mailinator.com>
2015-07-23 11:37:36 -0400

    install script

---
+++ cinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+# cleanup
+rm -rf platforms/
+rm -rf plugins/
+
+read -p "Which platforms do you want to build? (android ios): " platforms
+platforms=${platforms:-"android ios"}
+
+# install platforms and plugin
+echo "installing platforms "$platforms
+cordova platform add $platforms
+
+# reinstall specific plugins
+#cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="$FACEBOOK_APP_ID" --variable APP_NAME="$FACEBOOK_APP_NAME"
+cordova plugin add plugin.google.maps --variable API_KEY_FOR_ANDROID="$GMAPS_API_KEY_FOR_ANDROID" --variable API_KEY_FOR_IOS="$GMAPS_API_KEY_FOR_IOS"
+cordova plugin add org.apache.cordova.splashscreen
+#
+cordova plugin add org.apache.cordova.device
+cordova plugin add org.apache.cordova.device-motion
+cordova plugin add org.apache.cordova.device-orientation
+cordova plugin add org.apache.cordova.geolocation
+cordova plugin add org.apache.cordova.console
+cordova plugin add com.ionic.keyboard
+cordova plugin add org.apache.cordova.inappbrowser
